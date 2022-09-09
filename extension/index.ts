7d9461e7b965b8c37fa7192a0eac54aae3beee20
Christian Alfoni <christianalfoni@gmail.com>
2022-09-09 07:42:53 +0000

    Latest version

--- extension/index.ts
+++ extension/index.ts
@@ -1,71 +1,63 @@
 #!/usr/bin/env node
-import express from 'express'
-import * as ws from 'ws'
-import fs from 'fs/promises'
-import * as path from 'path'
-import chokidar from 'chokidar'
+import express from "express";
+import * as ws from "ws";
+import fs from "fs/promises";
+import * as path from "path";
+import chokidar from "chokidar";
 
-const app = express()
-const port = 3000
-const rootPath = process.env.NODE_ENV === 'development' ? path.join(process.cwd(), 'demo') : process.cwd()
-const files: Record<string, { code: string }> = {}
-const wsServer = new ws.Server({ noServer: true });
+const app = express();
+const port = 3000;
+const rootPath =
+  process.env.NODE_ENV === "development"
+    ? path.join(process.cwd(), "demo")
+    : process.cwd();
+const files: Record<string, { code: string }> = {};
+const wsServer = new ws.WebSocketServer({ noServer: true });
 
 // One-liner for current directory
-chokidar.watch(rootPath, {
-    ignored: [/node_modules/]
-}).on('all', async (event, path) => {
+chokidar
+  .watch(rootPath, {
+    ignored: [/node_modules/, /^\..*/],
+  })
+  .on("all", async (event, path) => {
+    switch (event) {
+      case "add": {
+        const stat = await fs.stat(path);
 
-
-        switch (event) {
-            case 'add': {
-                const stat = await fs.stat(path)
-        
-                if (stat.isFile()) {
-                    const content = await fs.readFile(path)
-                    files[path.substring(rootPath.length)] = { code: content.toString()}
-                }
-                break
-            }
-            case 'change': {
-                const content = await fs.readFile(path)
-                console.log("WTF?", content.toString())
-                files[path.substring(rootPath.length)] = { code: content.toString()}
-                break
-            }
-            case 'unlink': {
-                delete files[path.substring(rootPath.length)]
-                break
-            }
+        if (stat.isFile()) {
+          const content = await fs.readFile(path);
+          files[path.substring(rootPath.length)] = { code: content.toString() };
         }
-        
-          const stringifiedFiles = JSON.stringify(files)
-        wsServer.clients.forEach((socket) => {
-            socket.send(stringifiedFiles)
-        })
-    
-  
+        break;
+      }
+      case "change": {
+        const content = await fs.readFile(path);
+        files[path.substring(rootPath.length)] = { code: content.toString() };
+        break;
+      }
+      case "unlink": {
+        delete files[path.substring(rootPath.length)];
+        break;
+      }
+    }
+
+    const stringifiedFiles = JSON.stringify(files);
+    wsServer.clients.forEach((socket) => {
+      socket.send(stringifiedFiles);
+    });
+  });
+
+wsServer.on("connection", (socket) => {
+  socket.send(JSON.stringify(files));
 });
 
-wsServer.on('connection', socket => {
-
-  socket.send(JSON.stringify(files))
+app.get("/ui.js", async (_, res) => {
+  res.setHeader("Content-Type", "application/javascript");
+  res.send((await fs.readFile(`${__dirname}/ui.js`)).toString());
 });
 
-
-app.get('/ui.js', async (_, res) => {
-    res.setHeader('Content-Type', 'application/javascript')
-    res.send((await fs.readFile(`${__dirname}/ui.js`)).toString())
-})
-
-/*
-app.ws('/', () => {
-  
-})
-*/
-
-app.get('/', (_, res) => {
-    res.setHeader('Content-Type', 'text/html')
+app.get("/", (_, res) => {
+  res.setHeader("Content-Type", "text/html");
   res.send(`<!DOCTYPE html>
 <html>
   <head>
@@ -77,15 +69,15 @@ app.get('/', (_, res) => {
     <script src="ui.js"></script>
   </body>
 </html>
-`)
-})
+`);
+});
 
 const server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-server.on('upgrade', (request, socket, head) => {
-  wsServer.handleUpgrade(request, socket, head, socket => {
-    wsServer.emit('connection', socket, request);
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
   });
 });
--- ui/index.ts
+++ ui/index.ts
@@ -1,33 +1,22 @@
-import { SandpackClient } from '@codesandbox/sandpack-client'
+import { SandpackClient } from "@codesandbox/sandpack-client";
 
 document.body.innerHTML = `
 <iframe id="iframe" style="border:0;width:100%;height:100%;"/>
     
-`
+`;
 
-const ws = new WebSocket('ws://localhost:3000')
+const ws = new WebSocket("ws://localhost:3000");
 
-
-
-
-
-
-ws.onopen = () => {
-    console.log("HEEEY")
-}
-
-let client: SandpackClient
+let client: SandpackClient;
 
 ws.onmessage = (msg) => {
-    const files = JSON.parse(msg.data)
-    console.log(files)
-    if (client) {
-      client.updatePreview({files})
-    } else {
-        client = new SandpackClient('#iframe', {
-        files
-    })  
-    }
-    
-
-}
+  const files = JSON.parse(msg.data);
+  console.log(files);
+  if (client) {
+    client.updatePreview({ files });
+  } else {
+    client = new SandpackClient("#iframe", {
+      files,
+    });
+  }
+};
